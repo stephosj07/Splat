6622641be6892af083824c652e1e738605b3c889
Jan van Heusde <janvheusde@gmail.com>
2021-11-16 15:39:44 -0500

    seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CampingSpot.destroy_all
+User.destroy_all
+
+user1 = User.create!(email: 'christina_plants@gmail.com', password: '123456', first_name: 'Christina', last_name: 'aguilera')
+user2 = User.create!(email: 'joancvisca@gmail.com', password: '123456', first_name: 'Joan', last_name: 'visca')
+user3 = User.create!(email: 'andyplat@gmail.com', password: '123456', first_name: 'Andy', last_name: 'plat')
+user4 = User.create!(email: 'michealsmith@gmail.com', password: '123456', first_name: 'Micheal', last_name: 'Smith')
+
+CampingSpot.create!(location: '302 banff avenue', price: 20, capacity: '4 vans, 6 tents', description: 'back garden right in the hart of Banff with a view on the montains', host: user1)
+CampingSpot.create!(location: '14 saint petersburg avenue', price: 14, capacity: '6 vans, 2 tents', description: 'lovely land in the south of france', host: user4)
+CampingSpot.create!(location: '206 sherbrook road', price: 20, capacity: '3 vans, 8 tents', description: 'spot in montreal', host: user2)
+CampingSpot.create!(location: '36 prince street', price: 6, capacity: '10 vans, 8 tents', description: 'big peace of land on a budget', host: user3 )
