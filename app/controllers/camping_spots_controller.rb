a32dd8c9efa8a2473a172ef678d77976fa0a0e23
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-16 11:53:34 -0500

    Created base actions for controller and added routes.

---
+++ app/controllers/camping_spots_controller.rb
@@ -0,0 +1,32 @@
+class CampingSpotsController < ApplicationController
+  before_action :set_camping_spot, only: [:show, :edit, :update, :destroy]
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_camping_spot
+  end
+
+  def camping_spot_params
+  end
+end
---
+++ test/controllers/camping_spots_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CampingSpotsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
