57c274d4232a23a8acd90dd57e8ad0ce5cbb9a7c
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-19 11:13:41 -0500

    Added Review model and migration, and associations/validations.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :camping_spot
   belongs_to :visitor, foreign_key: :visitor_id, class_name: 'User'
+  has_many :reviews, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_210257) do
+ActiveRecord::Schema.define(version: 2021_11_19_155836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,15 @@ ActiveRecord::Schema.define(version: 2021_11_18_210257) do
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -89,4 +98,5 @@ ActiveRecord::Schema.define(version: 2021_11_18_210257) do
   add_foreign_key "bookings", "camping_spots"
   add_foreign_key "bookings", "users", column: "visitor_id"
   add_foreign_key "camping_spots", "users", column: "host_id"
+  add_foreign_key "reviews", "bookings"
 end
