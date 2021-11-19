bcc2e7576f6aee006d2337b8f6296ca3b848706e
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-19 14:17:43 -0500

    Fixed something.

--- app/javascript/plugins/init_typed.js
+++ app/javascript/plugins/init_typed.js
@@ -1,12 +1,13 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  console.log("hello world")
-  new Typed('#banner-typed-text', {
-    strings: ["Find your overnight camping spot!", "Anywhere in the world"],
-    typeSpeed: 60,
-    loop: true
-  });
+  if (document.getElementById('banner-typed-text')) {
+    new Typed('#banner-typed-text', {
+      strings: ["Find your overnight camping spot!", "Anywhere in the world"],
+      typeSpeed: 60,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
